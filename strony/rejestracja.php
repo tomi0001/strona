3e0aaa745b2c6e9fe11cdea69d5be642a4c319e3
tomi0001 <tomi0001@gmail.com>
2016-02-12 16:42:07 +0100

    Create rejestracja.php

---
+++ strony/rejestracja.php
@@ -0,0 +1,27 @@
+<div class=tytul2><font class=duza>Rejestracja</a></div>
+
+<form method=post>
+<table width=500 align=center>
+<tr>
+<td class=reje><div class=tytul2><font class=normalna>Twój login</font></div></td><td class=reje> <input type=text id=login></td>
+</tr>
+
+<tr>
+<td class=reje><div class=tytul2><font class=normalna>Twoje haslo</font></div></td><td class=reje> <input type=password id=haslo></td>
+</tr>
+
+<tr>
+<td class=reje><div class=tytul2><font class=normalna>Wpisz jeszcze raz swoje haslo</font></div></td><td class=reje> <input type=password id=haslo2></td>
+</tr>
+
+<tr>
+<td class=reje colspan=2><div class=tytul2> <input type = "button" value ="Zarejestruj"
+    onclick = "getData('zarejestruj.php',
+    'targetDiv')"></td>
+</tr>
+
+<tr>
+<td class=reje colspan=2><div class=tytul3 id =targetDiv></div></td>
+</tr>
+
+</table>
