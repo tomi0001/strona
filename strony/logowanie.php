7d7c957efdd13268ab2e94a2ea39c1b90ac5f9de
tomi0001 <tomi0001@gmail.com>
2016-02-12 16:41:15 +0100

    Create logowanie.php

---
+++ strony/logowanie.php
@@ -0,0 +1,23 @@
+<div class=tytul2><font class=duza>Zaloguj się </a></div>
+
+<form method=post action=./index.php?pozycja=zaloguj>
+<table width=500 align=center>
+<tr>
+<td class=reje><div class=tytul2><font class=normalna>Twój login</font></div></td><td class=reje> <input type=text name=login></td>
+</tr>
+
+<tr>
+<td class=reje><div class=tytul2><font class=normalna>Twoje haslo</font></div></td><td class=reje> <input type=password name=haslo></td>
+</tr>
+
+<tr>
+<td class=reje><div class=tytul2><font class=normalna>Czy zapamiętać</font></div></td><td class=reje> <input type=checkbox name=czy></td>
+</tr>
+
+<tr>
+<td class=reje colspan=2><div class=tytul2> <input type=submit value=zaloguj></td>
+</tr>
+
+
+
+</table>
